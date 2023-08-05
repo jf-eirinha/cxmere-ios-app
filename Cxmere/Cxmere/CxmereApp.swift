873f19cc740d5a2795138c56631e7ba19054a60f
João Eirinha <joaoeirinh@gmail.com>
2023-08-05 11:59:43 +0100

    xcode ios starter

---
+++ Cxmere/Cxmere/CxmereApp.swift
@@ -0,0 +1,17 @@
+//
+//  CxmereApp.swift
+//  Cxmere
+//
+//  Created by João Eirinha on 05/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct CxmereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
