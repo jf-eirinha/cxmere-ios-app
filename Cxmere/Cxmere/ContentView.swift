39654b3036ec9963260ab380f69daa31eff5899b
João Eirinha <joaoeirinh@gmail.com>
2023-08-05 15:19:34 +0100

    POC for image picker

--- Cxmere/Cxmere/ContentView.swift
+++ Cxmere/Cxmere/ContentView.swift
@@ -6,16 +6,43 @@
 //
 
 import SwiftUI
+import PhotosUI
+
+@MainActor
+final class ContentViewViewModel: ObservableObject {
+    @Published private(set) var selectedImage: UIImage? = nil
+    @Published var imageSelection: PhotosPickerItem? = nil {
+        didSet {
+            setImage(from: imageSelection)
+        }
+    }
+    
+    private func setImage(from selection: PhotosPickerItem?) {
+        guard let selection else { return }
+        
+        Task {
+            if let data = try? await selection.loadTransferable(type: Data.self) {
+                if let uiImage = UIImage(data: data){
+                    selectedImage = uiImage
+                    return
+                }
+            }
+        }
+    }
+}
 
 struct ContentView: View {
+    @StateObject private var viewModel = ContentViewViewModel()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+        if let image = viewModel.selectedImage {
+            Image(uiImage: image).resizable().frame(width: 300, height: 300).cornerRadius(10)
         }
-        .padding()
+        
+        PhotosPicker("Pick an image", selection: $viewModel.imageSelection, matching: .images)
+        }
     }
 }
 
